7a021f94047ec16a3b112d017ec94fe5bb0a5a01
Aidan Cole <aidanjcole@gmail.com>
2022-02-24 00:00:39 -0500

    excercise 1.11 iterative version of recursive function

--- excercise 1 11.rkt
+++ excercise 1 11.rkt
@@ -1,7 +1,10 @@
 #lang sicp
 
 (define (f n)
+  (define (f-rec a b c counter)
+    (if (< counter 0)
+        a
+        (f-rec (+ a (* 2 b) (* 3 c)) a b (- counter 1))))
   (if (< n 3)
       n
-      (+ (f (- n 1)) (* 2 (f (- n 2))) (* 3 (f (- n 3))))))
-
+      (f-rec 2 1 0 (- n 3))))
