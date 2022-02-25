2b65eadb68d026a42154575760382eb21c0395aa
Aidan Cole <aidanjcole@gmail.com>
2022-02-25 06:51:47 -0500

    1.2.4 fast-expt recursive procedure working and tested

---
+++ fast-expt.rkt
@@ -0,0 +1,9 @@
+#lang sicp
+
+(define (fast-expt b n)
+  (define (even? n)
+    (= (remainder n 2) 0))
+  (define (square x) (* x x))
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
